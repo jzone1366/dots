602e2557bd276d895058af44d317f6afd96ff5e6
Joshua Zone <joshua.zone@playvs.com>
2022-01-26 10:58:37 -0500

    Update nvim options.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,15 +2,19 @@ local wezterm = require('wezterm')
 
 return {
   font = wezterm.font('GoMono Nerd Font'),
-  font_size = 13,
+  font_size = 13.3,
 
   color_scheme = "TokyoNight",
   hide_tab_bar_if_only_one_tab = true,
+
+  default_cursor_style = "SteadyBar",
 
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
-  }
+  },
+
+  line_height = 1.0,
 }
