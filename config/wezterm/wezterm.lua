e8ed3ae617ded2efe1ca47334a9e7bb0855598ba
Joshua Zone <joshua.zone@playvs.com>
2022-02-18 14:37:15 -0500

    Update color schemes.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require('wezterm')
 
 return {
-  font = wezterm.font('GoMono Nerd Font'),
-  font_size = 13.3,
+  --font = wezterm.font('GoMono Nerd Font'),
+  --font = wezterm.font('FiraCode Nerd Font'),
+  font = wezterm.font('Hack Nerd Font'),
+  font_size = 13.5,
 
   color_scheme = "RosePine",
   hide_tab_bar_if_only_one_tab = true,
