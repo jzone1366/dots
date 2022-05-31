ae490789cded6920d3da98db1318b8f20deaf127
Joshua Zone <jzone1229@gmail.com>
2022-05-31 11:31:41 -0400

    Update nvim-tree setup to use new setup.

--- nvim/lua/zone/plugins/nvim-tree/init.lua
+++ nvim/lua/zone/plugins/nvim-tree/init.lua
@@ -3,32 +3,19 @@ local icons = require('zone.theme.icons')
 local augroup_name = 'ZoneNvimNvimTree'
 local group = vim.api.nvim_create_augroup(augroup_name, { clear = true })
 
--- settings
-g.nvim_tree_git_hl = 1
-g.nvim_tree_refresh_wait = 300
-
-g.nvim_tree_special_files = {}
-
-g.nvim_tree_icons = {
-  default = '',
-  symlink = icons.symlink,
-  git = icons.git,
-  folder = icons.folder,
-
-  lsp = {
-    hint = icons.hint,
-    info = icons.info,
-    warning = icons.warn,
-    error = icons.error,
-  },
-}
-
-g.nvim_tree_respect_buf_cwd = 1
-
--- set up args
+-- ARGUMENTS FOR SETUP.
+-- SOME OF THESE ARE THE DEFAULT OPTIONS, JUST HERE FOR BREVITY.
 local args = {
+  respect_buf_cwd = true,
   diagnostics = {
     enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = icons.hint,
+      info = icons.info,
+      warning = icons.warn,
+      error = icons.error,
+    },
   },
   ignore_ft_on_setup = {
     'startify',
@@ -44,7 +31,22 @@ local args = {
     relativenumber = true,
   },
   git = {
+    enable = true,
     ignore = true,
+    timeout = 400,
+  },
+  renderer = {
+    highlight_git = true,
+    special_files = {},
+    icons = {
+      webdev_colors = true,
+      glyphs = {
+        default = '',
+        symlink = icons.symlink,
+        git = icons.git,
+        folder = icons.folder,
+      },
+    },
   },
 }
 
