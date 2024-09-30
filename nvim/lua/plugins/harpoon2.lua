3e9df4da88c6aa154e31b41fd259469c3ccc20d0
Joshua Zone <jzone@ql2.com>
2024-09-30 16:02:32 -0400

    update keys

--- nvim/lua/plugins/harpoon2.lua
+++ nvim/lua/plugins/harpoon2.lua
@@ -32,5 +32,15 @@ return {
 
         map("n", "<leader>th", function() toggle_telescope(harpoon:list()) end,
             { desc = "Open harpoon window" })
+        map("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
+
+        map("n", "<C-1>", function() harpoon:list():select(1) end, { desc = "Select harpoon file 1" })
+        map("n", "<C-2>", function() harpoon:list():select(2) end, { desc = "Select harpoon file 2" })
+        map("n", "<C-3>", function() harpoon:list():select(3) end, { desc = "Select harpoon file 3" })
+        map("n", "<C-4>", function() harpoon:list():select(4) end, { desc = "Select harpoon file 4" })
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        map("n", "<C-S-P>", function() harpoon:list():prev() end)
+        map("n", "<C-S-N>", function() harpoon:list():next() end)
     end
 }
--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -6,46 +6,54 @@ local M = {}
 M.leader = { key = ',', mods = 'CTRL', timeout_milliseconds = 1500 }
 
 M.keys = {
-  -- LEADER+SHIFT for TAB Movement
-  { key = 'l',     mods = "CMD|SHIFT", action = act.ActivateTabRelative(1), },
-  { key = 'h',     mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1), },
+    -- LEADER+SHIFT for TAB Movement
+    { key = 'l',     mods = "CMD|SHIFT", action = act.ActivateTabRelative(1), },
+    { key = 'h',     mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1), },
 
-  -- LEADER for PANE Movement
-  { key = 'l',     mods = "LEADER", action = act.ActivatePaneDirection 'Right', },
-  { key = 'h',     mods = "LEADER", action = act.ActivatePaneDirection 'Left', },
-  { key = 'j',     mods = "LEADER", action = act.ActivatePaneDirection 'Down', },
-  { key = 'k',     mods = "LEADER", action = act.ActivatePaneDirection 'Up', },
+    -- LEADER for PANE Movement
+    { key = 'l',     mods = "LEADER",    action = act.ActivatePaneDirection 'Right', },
+    { key = 'h',     mods = "LEADER",    action = act.ActivatePaneDirection 'Left', },
+    { key = 'j',     mods = "LEADER",    action = act.ActivatePaneDirection 'Down', },
+    { key = 'k',     mods = "LEADER",    action = act.ActivatePaneDirection 'Up', },
 
-  -- splitting
-  { key = '-',     mods = "LEADER", action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
-  { key = '=',     mods = "LEADER", action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    -- splitting
+    { key = '-',     mods = "LEADER",    action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+    { key = '=',     mods = "LEADER",    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
 
-  { key = 'z',     mods = 'LEADER', action = act.TogglePaneZoomState, },
-  { key = 'Space', mods = 'LEADER', action = act.RotatePanes "Clockwise" },
-  { key = '0',     mods = 'LEADER', action = act.PaneSelect { mode = 'SwapWithActive', } },
-  { key = 'Enter', mods = 'LEADER', action = act.ActivateCopyMode, },
+    { key = 'z',     mods = 'LEADER',    action = act.TogglePaneZoomState, },
+    { key = 'Space', mods = 'LEADER',    action = act.RotatePanes "Clockwise" },
+    { key = '0',     mods = 'LEADER',    action = act.PaneSelect { mode = 'SwapWithActive', } },
+    { key = 'Enter', mods = 'LEADER',    action = act.ActivateCopyMode, },
 
-  -- Launcher
-  { key = 'm',     mods = 'LEADER', action = act.ShowLauncher},
-  { key = 's',     mods = 'LEADER', action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
-  { key = 't',     mods = 'LEADER', action = act.ShowLauncherArgs { flags = 'FUZZY|TABS' } },
-  { key = 'w',      mods = 'LEADER', action = act.PromptInputLine {
-    description = wezterm.format {
-      { Attribute = { Intensity = 'Bold' } },
-      { Foreground = { AnsiColor = 'Fuchsia' } },
-      { Text = 'Enter name for Workspace:' },
+    -- Launcher
+    { key = 'm',     mods = 'LEADER',    action = act.ShowLauncher },
+    { key = 's',     mods = 'LEADER',    action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
+    { key = 't',     mods = 'LEADER',    action = act.ShowLauncherArgs { flags = 'FUZZY|TABS' } },
+    {
+        key = 'w',
+        mods = 'LEADER',
+        action = act.PromptInputLine {
+            description = wezterm.format {
+                { Attribute = { Intensity = 'Bold' } },
+                { Foreground = { AnsiColor = 'Fuchsia' } },
+                { Text = 'Enter name for Workspace:' },
+            },
+            action = wezterm.action_callback(function(window, pane, line)
+                if line then
+                    window:perform_action(
+                        act.SwitchToWorkspace {
+                            name = line,
+                        },
+                        pane
+                    )
+                end
+            end),
+        },
     },
-    action = wezterm.action_callback(function(window, pane, line)
-      if line then
-        window:perform_action(
-          act.SwitchToWorkspace {
-            name = line,
-          },
-          pane
-        )
-      end
-    end),
-  }, },
+
+    -- Remove default keybindings
+    { key = 'n', mods = "CTRL|SHIFT", action = act.DisableDefaultAssignment },
+    { key = 'n', mods = "CTRL|SHIFT", action = act.ActivateCommandPalette }
 }
 
 return M
