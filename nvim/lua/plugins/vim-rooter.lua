76bfbd8c9e413251a15576be58ce1d699c8f7a60
Joshua Zone <jzone@ql2.com>
2024-10-04 12:32:56 -0400

    Remove vim-rooter.

--- nvim/lua/plugins/vim-rooter.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'airblade/vim-rooter',
-    event = 'VeryLazy',
-    config = function()
-        vim.g.rooter_patterns = { '.git', 'package.json', '_darcs', '.bzr', '.svn', 'Makefile' } -- Default
-        vim.g.rooter_silent_chdir = 1
-        vim.g.rooter_resolve_links = 1
-    end,
-}
