1869d9957f27e3a45ec48c9760c19f3aa2e2c0cc
Joshua Zone <zonejm@LittleBlackBook.local>
2026-01-19 21:15:07 -0500

    Add committia.vim to see changelogs in commitmessages;

--- nvim/plugin/40_plugins.lua
+++ nvim/plugin/40_plugins.lua
@@ -631,3 +631,30 @@ require('treewalker').setup({
   highlight_duration = 250,
   highlight_group = 'CursorLine',
 })
+
+MiniDeps.add({ source = 'rhysd/committia.vim' })
+
+-- See: https://github.com/rhysd/committia.vim#variables
+vim.g.committia_min_window_width = 30
+vim.g.committia_edit_window_width = 100
+vim.g.committia_use_singlecolumn = 'always'
+
+vim.g.committia_hooks = {
+  edit_open = function()
+    vim.cmd.resize(25)
+    local opts = {
+      buffer = vim.api.nvim_get_current_buf(),
+      silent = true,
+    }
+    local function map(mode, lhs, rhs)
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+    map('n', 'q', '<cmd>quit<CR>')
+    map('i', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
+    map('i', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
+    map('i', '<C-f>', '<Plug>(committia-scroll-diff-down-page)')
+    map('i', '<C-b>', '<Plug>(committia-scroll-diff-up-page)')
+    map('i', '<C-j>', '<Plug>(committia-scroll-diff-down)')
+    map('i', '<C-k>', '<Plug>(committia-scroll-diff-up)')
+  end,
+}
